ef0a9df2d468b03e0bf537de687c183ffb9d58fc
Wei Zang <richzw@gmail.com>
2013-02-19 19:01:49 -0800

    Create StringHash.h

---
+++ StringHash.h
@@ -0,0 +1,35 @@
+#ifndefine __STRING_HASH__
+#define __STRING_HASH__
+
+#include <string>
+using namespace std;
+
+#define MAXTABLELEN 1024    
+//////////////////////////////////////////////////////////////////////////  
+ 
+typedef struct  _HASHTABLE
+{  
+  	long nHashA;  
+	long nHashB;  
+	bool bExists;  
+}HASHTABLE, *PHASHTABLE ;  
+
+class StringHash
+{
+public:
+	StringHash(const long nTableLength = MAXTABLELEN);
+	~StringHash(void);
+private:  
+	unsigned long cryptTable[0x500];  
+	unsigned long m_tablelength;    
+	HASHTABLE *m_HashIndexTable; 
+private:
+	void InitCryptTable();                                             
+	unsigned long HashString(const string& lpszString, unsigned long dwHashType);    
+public:
+	bool Hash(string url);
+	unsigned long Hashed(string url);    
+	
+	unsigned long GetTableLength(void) { return m_tablelength; }
+};
+#endif
