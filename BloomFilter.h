dc15f71c9adbe81609d643bbbc158dc587be4bfa
Wei Zang <richzw@gmail.com>
2013-02-25 14:22:12 +0800

    Update BloomFilter.h

--- BloomFilter.h
+++ BloomFilter.h
@@ -5,21 +5,6 @@ using namespace std;
 #include <openssl/md5.h>
 #include "itoa.h"
 
-class Unpacked_BOOL{
-  bool imp;
-		
-public:
-	Unpacked_BOOL(bool b=false):imp(b){}
-	Unpacked_BOOL(int b):imp(b){}
-	Unpacked_BOOL(const Unpacked_BOOL& b):imp(b.imp){}
-
-	operator const bool&() const { return imp; }
-	operator bool&() { return imp; }
-
-	bool& operator=(bool b) { return imp = b; }
-	bool& operator=(int b) { return imp = b; }
-	bool& operator=(const Unpacked_BOOL& b) { return imp = b.imp; }
-};
 
 class BloomFilter{
 private:
